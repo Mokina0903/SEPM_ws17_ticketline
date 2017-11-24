49e83452546d5c3c74eb6a955767468fa770db2c
Verena Renner <e01527272@student.tuwien.ac.at>
2017-11-24 10:11:20 +0100

    Added fields to detailedNewsController

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/news/DetailedNewsController.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/news/DetailedNewsController.java
@@ -1,4 +1,16 @@
 package at.ac.tuwien.inso.sepm.ticketline.client.gui.news;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class DetailedNewsController {
+    @FXML
+    public Label lblDate;
+    @FXML
+    public Label lblTitle;
+    @FXML
+    public Label lblText;
+    @FXML
+    public ImageView newsImageViews;
 }
