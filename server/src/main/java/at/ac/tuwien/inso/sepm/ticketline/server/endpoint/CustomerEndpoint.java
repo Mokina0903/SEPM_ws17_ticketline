801e92ed3add57ed2b64e3e07e827d6bc160fbb0
Verena Renner <e01527272@student.tuwien.ac.at>
2017-12-14 13:39:36 +0100

    Added findById funktion within the Customer Endpoint

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/endpoint/CustomerEndpoint.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/endpoint/CustomerEndpoint.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.inso.sepm.ticketline.server.endpoint;
 
 import at.ac.tuwien.inso.sepm.ticketline.rest.customer.CustomerDTO;
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.mapper.customer.CustomerMapper;
+import at.ac.tuwien.inso.sepm.ticketline.server.exception.InvalidIdException;
 import at.ac.tuwien.inso.sepm.ticketline.server.service.CustomerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -26,10 +27,22 @@ public class CustomerEndpoint {
         this.customerService = service;
     }
 
-    @RequestMapping(value= "/{id}/{pageIndex}/{customerPerPage}",method = RequestMethod.GET)
-    @ApiOperation(value = "Get list of simple news entries")
-    public List<CustomerDTO> findAll(@PathVariable("id") Long id, @PathVariable("pageIndex")int pageIndex, @PathVariable("customerPerPage")int customerPerPage){
+    @RequestMapping(value= "/pageIndex}/{customerPerPage}", method = RequestMethod.GET)
+    @ApiOperation(value = "Get list of customer entries")
+    public List<CustomerDTO> findAll(@PathVariable("pageIndex")int pageIndex, @PathVariable("customerPerPage")int customerPerPage){
         PageRequest request = new PageRequest(pageIndex, customerPerPage, Sort.Direction.ASC, "surname");
         return customerMapper.customerToCustomerDTO(customerService.findAll(request));
     }
+    @RequestMapping(value= "/{id}",method = RequestMethod.GET)
+    @ApiOperation(value = "Get one spezific customer entry")
+    public CustomerDTO findById(@PathVariable("id") Long id){
+        try {
+            return customerMapper.customerToCustomerDTO(customerService.findOneById(id));
+        } catch (InvalidIdException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 }
