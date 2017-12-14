296e56e0278162e4626553ba5d6d5bd2aa11021e
01527305 <dav-it@outlook.com>
2017-12-14 15:30:16 +0100

    UserDialogController:  + Add Buttons/Lables/Fields as Variable  + Add Codestubs for saveUserData, goBackWithoutSave and update

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/user/UserDialogController.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/user/UserDialogController.java
@@ -1,17 +1,16 @@
 package at.ac.tuwien.inso.sepm.ticketline.client.gui.user;
 
+import at.ac.tuwien.inso.sepm.ticketline.client.gui.LocalizationObserver;
 import at.ac.tuwien.inso.sepm.ticketline.client.gui.MainController;
 import at.ac.tuwien.inso.sepm.ticketline.client.gui.news.NewsController;
+import at.ac.tuwien.inso.sepm.ticketline.client.util.BundleManager;
 import at.ac.tuwien.inso.sepm.ticketline.rest.user.DetailedUserDTO;
 import at.ac.tuwien.inso.springfx.SpringFxmlLoader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -21,16 +20,25 @@ public class UserDialogController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
     @FXML
-    public TextField usernaeTF;
+    public TextField usernameTF;
     @FXML
-    public TextField passwordTF;
+    public PasswordField passwordPF;
     @FXML
-    public TextField passwordConfirmTF;
+    public PasswordField passwordConfirmPF;
     @FXML
     public ComboBox roleCombo;
     @FXML
     public Button saveBtn;
-
+    @FXML
+    public Button backBtn;
+    @FXML
+    public Label usernameLb;
+    @FXML
+    public Label passwordLb;
+    @FXML
+    public Label passwordConfirmLb;
+    @FXML
+    public Label roleLb;
 
 
     private MainController mainController;
@@ -60,8 +68,30 @@ public class UserDialogController {
     public void saveUserData(ActionEvent actionEvent) {
         //TODO: save the manipulated DTO to the Database. It should not make a difference either an new user or an exsisting user is updated
 
+        userController.loadUsers();
         userController.getUserTab().setContent(oldContent);
     }
 
+    @FXML
+    public void goBackWithoutSave(ActionEvent actionEvent) {
+        //TODO: save the manipulated DTO to the Database. It should not make a difference either an new user or an exsisting user is updated
 
+        userController.loadUsers();
+        userController.getUserTab().setContent(oldContent);
+    }
+
+    public void update() {
+        // TODO: How to update also this fields?
+
+        usernameLb.setText(BundleManager.getBundle().getString("authenticate.userName"));
+        passwordLb.setText(BundleManager.getBundle().getString("authenticate.password"));
+        passwordConfirmLb.setText(BundleManager.getBundle().getString("user.confirm"));
+        roleLb.setText(BundleManager.getBundle().getString("user.role"));
+
+        passwordPF.setPromptText("authenticate.password");
+        passwordConfirmPF.setPromptText("authenticate.password");
+
+        saveBtn.setText(BundleManager.getBundle().getString("general.save"));
+        backBtn.setText(BundleManager.getBundle().getString("general.back"));
+    }
 }
