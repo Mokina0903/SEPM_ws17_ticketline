36b947c8a9fa5c56292398e75209d0ed39a657ce
stefan0967530 <stefan@klausser.at>
2017-12-17 17:03:46 +0100

    added method to find upcoming events with pagination on server

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/service/EventService.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/service/EventService.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.inso.sepm.ticketline.server.service;
 
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.Event;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface EventService {
@@ -20,4 +21,13 @@ public interface EventService {
      * @return the event entry
      */
     Event findOne(Long id);
+
+
+    /**
+     * Find all upcoming events(that has not yet ended) ordered by Date ascending
+     *
+     * @param request pageRequest, which page and how many entries
+     * @return List of events
+     */
+    List<Event> findAllUpcomingAsc( Pageable request);
 }
