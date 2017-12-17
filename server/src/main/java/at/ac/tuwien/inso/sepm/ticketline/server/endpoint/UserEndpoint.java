96a665cfd4900a1364a5497b0c8db04b961aea9e
01527305 <dav-it@outlook.com>
2017-12-17 13:11:39 +0100

    UserEndpoint: Fixed Bug, user can block themself

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/endpoint/UserEndpoint.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/endpoint/UserEndpoint.java
@@ -7,14 +7,18 @@ import at.ac.tuwien.inso.sepm.ticketline.server.entity.User;
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.mapper.news.NewsMapper;
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.mapper.user.UserMapper;
 import at.ac.tuwien.inso.sepm.ticketline.server.exception.EmptyFieldException;
+import at.ac.tuwien.inso.sepm.ticketline.server.exception.IllegalValueException;
 import at.ac.tuwien.inso.sepm.ticketline.server.repository.UserRepository;
 import at.ac.tuwien.inso.sepm.ticketline.server.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import javax.lang.model.element.AnnotationValue;
 import java.util.List;
 import java.util.Optional;
 
@@ -145,6 +149,13 @@ public class UserEndpoint {
     @PreAuthorize("hasRole('ADMIN')")
     @ApiOperation(value = "Block a specific user entry")
     public void blockUser(@RequestBody String username) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String asked = ((UserDetails)principal).getUsername();
+
+        if (asked.trim().toUpperCase().equals(username.trim().toUpperCase())) {
+            throw new IllegalValueException("querist == username");
+        }
+
         User user = userService.findByUsername(username);
         userService.blockUser(user);
     }
