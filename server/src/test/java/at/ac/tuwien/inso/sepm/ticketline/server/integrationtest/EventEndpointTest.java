11f2c8612550a136ee9f0a6dc4c77fd0392e3b14
David <dav-it@outlook.com>
2018-01-02 17:05:11 +0100

    EventEndpointTest: Implemented Tests:  + publishEventUnauthorizedAsUser  + publishEventAsAdmin

--- server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/integrationtest/EventEndpointTest.java
+++ server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/integrationtest/EventEndpointTest.java
@@ -1,18 +1,98 @@
 package at.ac.tuwien.inso.sepm.ticketline.server.integrationtest;
 
+import at.ac.tuwien.inso.sepm.ticketline.rest.event.DetailedEventDTO;
+import at.ac.tuwien.inso.sepm.ticketline.rest.eventLocation.hall.DetailedHallDTO;
+import at.ac.tuwien.inso.sepm.ticketline.rest.news.DetailedNewsDTO;
+import at.ac.tuwien.inso.sepm.ticketline.server.entity.Event;
 import at.ac.tuwien.inso.sepm.ticketline.server.integrationtest.base.BaseIntegrationTest;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.core.Is.is;
 
 public class EventEndpointTest extends BaseIntegrationTest {
 
+    private static final String EVENT_ENDPOINT = "/event";
+
     @Test
     public void publishEventUnauthorizedAsUser() {
-        // TODO: Publish as User -> error
+        LocalDateTime start = LocalDateTime.of(2017, 11, 29, 12, 15, 0, 0);
+
+        DetailedHallDTO detailedHallDTO = DetailedHallDTO.builder()
+            .description("Hall Description")
+            .id(1L)
+            .build();
+
+
+        DetailedEventDTO detailedEventDTO = DetailedEventDTO.builder()
+            .startOfEvent(start)
+            .endOfEvent(start.plusHours(2))
+            .artistFirstname("artistFirstName")
+            .artistLastName("artistLastName")
+            .price(123L)
+            .description("Description")
+            .title("Title")
+            .hall(detailedHallDTO)
+            .build();
+
+        Response response = RestAssured
+            .given()
+            .contentType(ContentType.JSON)
+            .header(HttpHeaders.AUTHORIZATION, validUserTokenWithPrefix)
+            .body(detailedEventDTO)
+            .when().post(EVENT_ENDPOINT)
+            .then().extract().response();
+        Assert.assertThat(response.getStatusCode(), is(HttpStatus.FORBIDDEN.value()));
     }
 
     @Test
     public void publishEventAsAdmin() {
-        // TODO: Publish as admin -> Correct
+        setupDefaultLocation();
+
+        LocalDateTime start = LocalDateTime.of(2017, 11, 29, 12, 15, 0, 0);
+
+        DetailedHallDTO detailedHallDTO = DetailedHallDTO.builder()
+            .description("Hall Description")
+            .id(1L)
+            .build();
+
+
+        DetailedEventDTO detailedEventDTO = DetailedEventDTO.builder()
+            .startOfEvent(start)
+            .endOfEvent(start.plusHours(2))
+            .artistFirstname("artistFirstName")
+            .artistLastName("artistLastName")
+            .price(123L)
+            .description("Description")
+            .title("Title")
+            .hall(detailedHallDTO)
+            .build();
+
+        Response response = RestAssured
+            .given()
+            .contentType(ContentType.JSON)
+            .header(HttpHeaders.AUTHORIZATION, validAdminTokenWithPrefix)
+            .body(detailedEventDTO)
+            .when().post(EVENT_ENDPOINT)
+            .then().extract().response();
+        Assert.assertThat(response.getStatusCode(), is(HttpStatus.OK.value()));
+
+
+        detailedEventDTO = response.as(DetailedEventDTO.class);
+
+        System.out.println(detailedEventDTO.toString());
+
+        // TODO: David Compare Objects
+
     }
 
 }
