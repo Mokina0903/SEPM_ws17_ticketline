fb05b36d7cb57107e9fab7cf77e236da8ebd4bcb
David <dav-it@outlook.com>
2018-01-02 15:22:14 +0100

    Test CSV-Import: Created Class and added some TODOs

---
+++ server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/integrationtest/EventEndpointTest.java
@@ -0,0 +1,18 @@
+package at.ac.tuwien.inso.sepm.ticketline.server.integrationtest;
+
+import at.ac.tuwien.inso.sepm.ticketline.server.integrationtest.base.BaseIntegrationTest;
+import org.junit.Test;
+
+public class EventEndpointTest extends BaseIntegrationTest {
+
+    @Test
+    public void publishEventUnauthorizedAsUser() {
+        // TODO: Publish as User -> error
+    }
+
+    @Test
+    public void publishEventAsAdmin() {
+        // TODO: Publish as admin -> Correct
+    }
+
+}
