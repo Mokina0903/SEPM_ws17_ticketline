852629f55153d38f969dc8e97b6cc1fa9aaf5528
stefan0967530 <stefan@klausser.at>
2018-01-04 13:52:36 +0100

    Ticket: Implemented feature #22823 @10m + added Mapper

---
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/entity/mapper/ticket/TicketMapper.java
@@ -0,0 +1,18 @@
+package at.ac.tuwien.inso.sepm.ticketline.server.entity.mapper.ticket;
+
+import at.ac.tuwien.inso.sepm.ticketline.rest.ticket.TicketDTO;
+import at.ac.tuwien.inso.sepm.ticketline.server.entity.Ticket;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface TicketMapper {
+
+    Ticket ticketDTOtoTicket( TicketDTO ticketDTO);
+
+    TicketDTO ticketToTicketDTO(Ticket ticket);
+
+    List<TicketDTO> ticketToTicketDTO( List<Ticket> all);
+
+}
