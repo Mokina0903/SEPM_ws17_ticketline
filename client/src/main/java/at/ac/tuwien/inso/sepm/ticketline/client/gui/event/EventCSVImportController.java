3cb5a3987b0c2ce06494a7bb44bc116b666aa378
Verena Renner <e01527272@student.tuwien.ac.at>
2018-01-07 18:24:12 +0100

    Added the update() method to update the language of the elements within the add event window. [Task #22190]

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/event/EventCSVImportController.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/event/EventCSVImportController.java
@@ -1,15 +1,22 @@
 package at.ac.tuwien.inso.sepm.ticketline.client.gui.event;
 
+import at.ac.tuwien.inso.sepm.ticketline.client.gui.LocalizationObserver;
+import at.ac.tuwien.inso.sepm.ticketline.client.gui.LocalizationSubject;
+import at.ac.tuwien.inso.sepm.ticketline.client.gui.TabElement;
 import at.ac.tuwien.inso.sepm.ticketline.client.gui.TabHeaderController;
+import at.ac.tuwien.inso.sepm.ticketline.client.util.BundleManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
+import org.controlsfx.glyphfont.FontAwesome;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EventCSVImportController {
+public class EventCSVImportController implements LocalizationObserver {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(at.ac.tuwien.inso.sepm.ticketline.client.gui.event.EventController.class);
     @FXML
@@ -22,8 +29,26 @@ public class EventCSVImportController {
     @FXML
     private TabHeaderController tabHeaderController;
 
+    @Autowired
+    private LocalizationSubject localizationSubject;
+
+    @FXML
+    void initialize(){
+        tabHeaderController.setIcon(FontAwesome.Glyph.FILM);
+        tabHeaderController.setTitle(BundleManager.getBundle().getString("event.addEvent"));
+        localizationSubject.attach(this);
+    }
+
    void EventCSVImportController(){
 
    }
+
+    @Override
+    public void update() {
+        tabHeaderController.setTitle(BundleManager.getBundle().getString("event.addEvent"));
+        btnAddFile.setText(BundleManager.getBundle().getString(" event.chooseYourInputFile"));
+        btnImport.setText(BundleManager.getBundle().getString("event.import"));
+
+    }
 
 }
