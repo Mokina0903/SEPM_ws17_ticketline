66e986b01580918ec31e47e364176d455bb18a59
stefan0967530 <stefan@klausser.at>
2018-01-08 16:01:38 +0100

    added TicketEndpointTest

---
+++ server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/integrationtest/TicketEndpointTest.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.inso.sepm.ticketline.server.integrationtest;
+
+import at.ac.tuwien.inso.sepm.ticketline.server.integrationtest.base.BaseIntegrationTest;
+
+public class TicketEndpointTest extends BaseIntegrationTest {
+
+    private static final String TICKET_ENDPOINT = "/tickets";
+    private static final String TICKET_EVENT_PATH = "/event";
+    private static final String TICKET_CUSTOMER_PATH = "/customer";
+
+}
