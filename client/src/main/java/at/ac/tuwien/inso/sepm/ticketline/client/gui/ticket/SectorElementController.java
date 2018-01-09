e0ddf581b7a2e494e022d68c01bd57598fb379ed
Verena Renner <e01527272@student.tuwien.ac.at>
2018-01-09 20:11:27 +0100

    Added two buttons to the sectorElement.fxml, one for increasing one for decreasing the seatcount. [Task #23013]

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/ticket/SectorElementController.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/ticket/SectorElementController.java
@@ -2,8 +2,14 @@ package at.ac.tuwien.inso.sepm.ticketline.client.gui.ticket;
 
 import at.ac.tuwien.inso.sepm.ticketline.rest.eventLocation.seat.SeatDTO;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import org.controlsfx.glyphfont.FontAwesome;
+import org.controlsfx.glyphfont.Glyph;
+import org.controlsfx.glyphfont.GlyphFont;
+import org.controlsfx.glyphfont.GlyphFontRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +23,13 @@ public class SectorElementController {
     public Label currentReservedTickets;
     @FXML
     public Label maxAnzFromTickets;
+    @FXML
+    public Button btnIncrease;
+    @FXML
+    public Button btnDecrease;
+
+    private GlyphFont fontAwesome = GlyphFontRegistry.font("FontAwesome");
+    private final int FONT_SIZE = 16;
 
 
     private HallplanController hallplanController;
@@ -24,6 +37,16 @@ public class SectorElementController {
     void initializeData(int reservedTickets, HallplanController hallplanController){
         currentReservedTickets.setText(String.valueOf(reservedTickets));
         this.hallplanController = hallplanController;
+
+        setButtonGraphic(btnDecrease, "PLUS", Color.DARKGRAY);
+        setButtonGraphic(btnIncrease, "MINUS", Color.DARKGRAY);
+    }
+
+    private void setButtonGraphic(Button button, String glyphSymbol, Color color) {
+        Glyph glyph = fontAwesome.create(FontAwesome.Glyph.valueOf(glyphSymbol));
+        glyph.setColor(color);
+        glyph.setFontSize(FONT_SIZE);
+        button.setGraphic(glyph);
     }
 
 }
