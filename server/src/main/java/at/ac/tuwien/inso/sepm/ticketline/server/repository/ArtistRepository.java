7d20a6385c199cd7c006b505398afda56ae28de4
David <dav-it@outlook.com>
2018-01-10 14:11:10 +0100

    ArtistRepository: Implemented findByArtistFirstNameAndArtistLastName()

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/ArtistRepository.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/ArtistRepository.java
@@ -2,9 +2,15 @@ package at.ac.tuwien.inso.sepm.ticketline.server.repository;
 
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
+
+    // TODO: Javadoc
+    @Query(value = "Select * from artist where artist_First_Name = :firstName and artist_Last_Name = :lastName", nativeQuery = true)
+    Artist findByArtistFirstNameAndArtistLastName(@Param("firstName") String firstName, @Param("lastName") String lastName);
 
 }
