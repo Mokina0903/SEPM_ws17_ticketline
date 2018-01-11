46293626b4d5e598d6cd40880a823fb3f5a605c7
Verena Renner <e01527272@student.tuwien.ac.at>
2018-01-11 14:37:24 +0100

    Linked the cutline to the hallplan.fxml. [Task #23064, IR2 Request]

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/ticket/HallplanCutlineController.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/ticket/HallplanCutlineController.java
@@ -1,5 +1,7 @@
 package at.ac.tuwien.inso.sepm.ticketline.client.gui.ticket;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -7,6 +9,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class HallplanCutlineController {
     private static final Logger LOGGER = LoggerFactory.getLogger(at.ac.tuwien.inso.sepm.ticketline.client.gui.ticket.TicketController.class);
+
+    @FXML
+    public Label lbPriceA;
+    @FXML
+    public Label lbPriceB;
+    @FXML
+    public Label lbPriceC;
+    @FXML
+    public Label lbPriceD;
+    @FXML
+    public Label lbPriceE;
+
+    void initializeData(){
+
+    }
 
 
 }
