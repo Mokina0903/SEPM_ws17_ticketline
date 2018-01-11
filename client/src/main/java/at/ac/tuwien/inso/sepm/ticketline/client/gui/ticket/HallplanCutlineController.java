9bf45ce8855adaccceed27c7cddf3654cd4b2af4
Verena Renner <e01527272@student.tuwien.ac.at>
2018-01-11 14:18:04 +0100

    Created the cutline for the hallplan. [Task #23064, IR2 Request]

---
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/ticket/HallplanCutlineController.java
@@ -0,0 +1,12 @@
+package at.ac.tuwien.inso.sepm.ticketline.client.gui.ticket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HallplanCutlineController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(at.ac.tuwien.inso.sepm.ticketline.client.gui.ticket.TicketController.class);
+
+
+}
