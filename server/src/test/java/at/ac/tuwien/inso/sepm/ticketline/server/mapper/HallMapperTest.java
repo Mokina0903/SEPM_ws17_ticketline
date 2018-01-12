fd32c837a59338bd2c62a3e0158fbb4d1feb888a
David <dav-it@outlook.com>
2018-01-12 16:00:13 +0100

    Tests-General: Add Todos in MapperTest

--- server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/mapper/HallMapperTest.java
+++ server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/mapper/HallMapperTest.java
@@ -1,4 +1,5 @@
 package at.ac.tuwien.inso.sepm.ticketline.server.mapper;
 
 public class HallMapperTest {
+    // TODO: Implement here Tests
 }
--- server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/mapper/TicketMapperTest.java
+++ server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/mapper/TicketMapperTest.java
@@ -1,4 +1,5 @@
 package at.ac.tuwien.inso.sepm.ticketline.server.mapper;
 
 public class TicketMapperTest {
+    // TODO: Implement here Tests
 }
