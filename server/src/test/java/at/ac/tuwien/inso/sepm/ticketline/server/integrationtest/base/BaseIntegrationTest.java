382748306527295498f5adc222f774df84529143
David <dav-it@outlook.com>
2018-01-14 17:40:27 +0100

    BaseIntegrationTest: Task #22030 Add Repositories/SetUpMethods for:  + Seat  + Customer  + Event  + Ticket

--- server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/integrationtest/base/BaseIntegrationTest.java
+++ server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/integrationtest/base/BaseIntegrationTest.java
@@ -1,16 +1,14 @@
 package at.ac.tuwien.inso.sepm.ticketline.server.integrationtest.base;
 
 import at.ac.tuwien.inso.sepm.ticketline.server.configuration.JacksonConfiguration;
-import at.ac.tuwien.inso.sepm.ticketline.server.entity.Artist;
-import at.ac.tuwien.inso.sepm.ticketline.server.entity.News;
-import at.ac.tuwien.inso.sepm.ticketline.server.entity.User;
+import at.ac.tuwien.inso.sepm.ticketline.server.entity.*;
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.eventLocation.Hall;
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.eventLocation.Location;
-import at.ac.tuwien.inso.sepm.ticketline.server.repository.ArtistRepository;
+import at.ac.tuwien.inso.sepm.ticketline.server.entity.eventLocation.Seat;
+import at.ac.tuwien.inso.sepm.ticketline.server.repository.*;
 import at.ac.tuwien.inso.sepm.ticketline.server.repository.Location.HallRepository;
 import at.ac.tuwien.inso.sepm.ticketline.server.repository.Location.LocationRepository;
-import at.ac.tuwien.inso.sepm.ticketline.server.repository.NewsRepository;
-import at.ac.tuwien.inso.sepm.ticketline.server.repository.UserRepository;
+import at.ac.tuwien.inso.sepm.ticketline.server.repository.Location.SeatRepository;
 import at.ac.tuwien.inso.sepm.ticketline.server.security.AuthenticationConstants;
 import at.ac.tuwien.inso.sepm.ticketline.server.service.UserService;
 import at.ac.tuwien.inso.sepm.ticketline.server.service.implementation.SimpleHeaderTokenAuthenticationService;
@@ -29,7 +27,10 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -72,6 +73,22 @@ public abstract class BaseIntegrationTest {
     protected static LocalDateTime EVENT_START =
         LocalDateTime.of(2017, 11, 29, 12, 15, 0, 0);
 
+    protected static final long TEST_CUSTOMER_ID = 1L;
+    protected static final long TEST_CUSTOMER_NUMBER = 9999L;
+    protected static final String  TEST_CUSTOMER_NAME = "Max";
+    protected static final String  TEST_CUSTOMER_SURNAME = "Mustermann";
+    protected static final String  TEST_CUSTOMER_MAIL = "Maxmustermann@gmail.com";
+    protected static final LocalDate TEST_CUSTOMER_BIRTHDATE = LocalDate.of(1950, 1, 1);
+    protected static final String  TEST_CUSTOMER_NAME_SUBSTRING = "muste";
+
+    protected static final Long SEAT_ID = 1L;
+    protected static final int SEAT_NR = 1;
+    protected static final char SEAT_SECTOR = 'a';
+    protected static final int SEAT_ROW = 1;
+
+    protected static final Long TICKET_RESERVATIONNR = 10001L;
+    protected static final Long TICKET_ID = 1L;
+    protected static final int TICKET_PRICE = 12300;
 
     @Value("${server.context-path}")
     private String contextPath;
@@ -105,14 +122,22 @@ public abstract class BaseIntegrationTest {
     protected HallRepository hallRepository;
 
     @Autowired
+    protected SeatRepository seatRepository;
+
+    @Autowired
     protected ArtistRepository artistRepository;
+
+    @Autowired
+    protected CustomerRepository customerRepository;
+
+    @Autowired
+    protected EventRepository eventRepository;
+
+    @Autowired
+    protected TicketRepository ticketRepository;
 
     protected String validUserTokenWithPrefix;
     protected String validAdminTokenWithPrefix;
-    protected Location location;
-    protected Hall hall;
-    protected Artist artist;
-
 
     @Before
     public void beforeBase() throws Exception {
@@ -120,6 +145,7 @@ public abstract class BaseIntegrationTest {
         setupDefaultUsers();
         setupDefaultLocation();
         setUpDefaultArtist();
+        setUpDefaultCustomers();
 
         RestAssured.baseURI = SERVER_HOST;
         RestAssured.basePath = contextPath;
@@ -202,6 +228,9 @@ public abstract class BaseIntegrationTest {
     }
 
     public void setupDefaultLocation(){
+        Location location;
+        Hall hall;
+
         location = Location.builder()
             .id(LOCATION_ID)
             .description(LOCATION_DESCRIPTION)
@@ -220,15 +249,102 @@ public abstract class BaseIntegrationTest {
             .location(location)
             .build();
         hall = hallRepository.save(hall);
+
+        Seat seat = Seat.builder()
+            .id(SEAT_ID)
+            .hall(hall)
+            .nr(SEAT_NR)
+            .sector(SEAT_SECTOR)
+            .row(SEAT_ROW)
+            .build();
+
+        seatRepository.save(seat);
+
+        /*
+        // Seats
+        int columns = 5;
+        int rows = 5;
+        int sectors = 3;
+        int secRows = columns / sectors;
+        List<Seat> seats = new ArrayList<>();
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                Seat seat = Seat.builder()
+                    .nr(col + 1)
+                    .row(row + 1)
+                    .sector((char) (((row) / secRows) + 97))
+                    .hall(hall)
+                    .build();
+                seats.add(seat);
+            }
+        }
+        */
     }
 
     public void setUpDefaultArtist() {
-        artist = Artist.builder()
+        Artist artist = Artist.builder()
             .id(ARTIST_ID)
             .artistFirstname(ARTIST_FIRSTNAME)
             .artistLastName(ARTIST_LASTNAME)
             .build();
 
         artist = artistRepository.save(artist);
+    }
+
+    public void setUpDefaultCustomers() {
+        Customer customer = Customer.builder()
+            .id(TEST_CUSTOMER_ID)
+            .knr(TEST_CUSTOMER_NUMBER)
+            .name(TEST_CUSTOMER_NAME)
+            .surname(TEST_CUSTOMER_SURNAME)
+            .mail(TEST_CUSTOMER_MAIL)
+            .birthDate(TEST_CUSTOMER_BIRTHDATE)
+            .build();
+        customerRepository.save(customer);
+    }
+
+    public void setUpDefaultEvent() {
+        Hall hall = hallRepository.findOne(HALL_ID);
+
+        List<Artist> artists = new ArrayList<>();
+
+        artists.add(artistRepository.findOne(ARTIST_ID));
+
+        Event event = Event.builder()
+            .id(EVENT_ID)
+            .startOfEvent(EVENT_START)
+            .endOfEvent(EVENT_START.plusHours(2))
+            .artists(artists)
+            .price(EVENT_PRICE)
+            .description(EVENT_DESCRIPTION)
+            .title(EVENT_TITLE)
+            .hall(hall)
+            .seatSelection(true)
+            .build();
+
+        eventRepository.save(event);
+    }
+
+    public void setUpDefaultEvent(LocalDateTime startOfEvent) {
+        Hall hall = hallRepository.findOne(HALL_ID);
+
+        List<Artist> artists = new ArrayList<>();
+
+        artists.add(artistRepository.findOne(ARTIST_ID));
+
+        Event event = Event.builder()
+            .id(EVENT_ID + 1)
+            .startOfEvent(startOfEvent)
+            .endOfEvent(startOfEvent.plusHours(2))
+            .artists(artists)
+            .price(EVENT_PRICE)
+            .description(EVENT_DESCRIPTION)
+            .title(EVENT_TITLE)
+            .hall(hall)
+            .seatSelection(true)
+            .build();
+
+        eventRepository.save(event);
     }
 }
