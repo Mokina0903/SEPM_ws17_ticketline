8327d70590568ccf5e118834685edcb1931ee45a
stefan0967530 <stefan@klausser.at>
2018-01-14 16:35:36 +0100

    added @Transactional refs #23142

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/InvoiceRepository.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/InvoiceRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice,Long>{
 
--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/TicketRepository.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/TicketRepository.java
@@ -10,11 +10,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket,Long>{
 
