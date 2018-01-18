7b85aa779591cdd24f123054a991485ab7c81820
Verena Renner <e01527272@student.tuwien.ac.at>
2018-01-19 00:03:59 +0100

    Added a basic structure to the fxml und created those fields within the controller. [Task #23287]

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/statistics/Top10Controller.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/statistics/Top10Controller.java
@@ -1,4 +1,22 @@
 package at.ac.tuwien.inso.sepm.ticketline.client.gui.statistics;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.CheckBox;
+
 public class Top10Controller {
+
+    @FXML
+    public BarChart barChartTop10;
+    @FXML
+    public CategoryAxis xAxis;
+    @FXML
+    public NumberAxis yAxis;
+
+
+    public void applyStatsToChart(ActionEvent actionEvent) {
+    }
 }
