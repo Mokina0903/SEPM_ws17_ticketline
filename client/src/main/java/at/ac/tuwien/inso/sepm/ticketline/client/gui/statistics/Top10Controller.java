4970cdac90c82c2564c420136561459efbb3df6c
Verena Renner <e01527272@student.tuwien.ac.at>
2018-01-19 00:38:38 +0100

    Edited the fxml, so that a kategory can be added to a combobox. [Task #23287]

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/statistics/Top10Controller.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/statistics/Top10Controller.java
@@ -1,13 +1,17 @@
 package at.ac.tuwien.inso.sepm.ticketline.client.gui.statistics;
 
+import at.ac.tuwien.inso.sepm.ticketline.client.gui.MainController;
 import at.ac.tuwien.inso.sepm.ticketline.client.gui.TabElement;
+import at.ac.tuwien.inso.springfx.SpringFxmlLoader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class Top10Controller extends TabElement {
@@ -18,11 +22,31 @@ public class Top10Controller extends TabElement {
     public CategoryAxis xAxis;
     @FXML
     public NumberAxis yAxis;
+    @FXML
+    public ComboBox comBoxKategory;
 
-    public Tab statisticsTab;
+    private MainController mainController;
+    private SpringFxmlLoader loader;
+    private Tab statisticsTab;
 
+    public Top10Controller(MainController mainController, SpringFxmlLoader loader) {
+
+        this.loader = loader;
+        this.mainController = mainController;
+    }
+
+    public void initializeData() {
+
+        xAxis.setLabel("Events");
+        yAxis.setLabel("Sales");
+        barChartTop10.setTitle("Top 10 Events");
+
+    }
 
     public void applyStatsToChart(ActionEvent actionEvent) {
+
+
+
     }
 
     @Override
