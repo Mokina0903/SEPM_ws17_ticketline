f922c2ca0be3f5e275e89c14933f4fc6beed4143
Florian <e1527080@student.tuwien.ac.at>
2018-01-20 16:51:38 +0100

    Add LocalizationSubject to the TicketController #22811

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/ticket/TicketController.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/ticket/TicketController.java
@@ -3,10 +3,7 @@ package at.ac.tuwien.inso.sepm.ticketline.client.gui.ticket;
 import at.ac.tuwien.inso.sepm.ticketline.client.exception.BlockedUserException;
 import at.ac.tuwien.inso.sepm.ticketline.client.exception.DataAccessException;
 import at.ac.tuwien.inso.sepm.ticketline.client.exception.SearchNoMatchException;
-import at.ac.tuwien.inso.sepm.ticketline.client.gui.LocalizationObserver;
-import at.ac.tuwien.inso.sepm.ticketline.client.gui.MainController;
-import at.ac.tuwien.inso.sepm.ticketline.client.gui.TabElement;
-import at.ac.tuwien.inso.sepm.ticketline.client.gui.TabHeaderController;
+import at.ac.tuwien.inso.sepm.ticketline.client.gui.*;
 import at.ac.tuwien.inso.sepm.ticketline.client.gui.customer.CustomerSearchFor;
 import at.ac.tuwien.inso.sepm.ticketline.client.rest.TicketRestClient;
 import at.ac.tuwien.inso.sepm.ticketline.client.service.TicketService;
@@ -31,6 +28,7 @@ import javafx.util.Callback;
 import org.controlsfx.glyphfont.FontAwesome;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -62,6 +60,9 @@ public class TicketController extends TabElement implements LocalizationObserver
 
     @FXML
     public Label lblStorno;
+
+    @Autowired
+    private LocalizationSubject localizationSubject;
 
 
 
@@ -111,6 +112,8 @@ public class TicketController extends TabElement implements LocalizationObserver
         tabHeaderController.setTitle("Tickets");
         lblStorno.setWrapText(true);
         lblStorno.setMaxWidth(100.0);
+        localizationSubject.attach(this);
+
 
     }
 
