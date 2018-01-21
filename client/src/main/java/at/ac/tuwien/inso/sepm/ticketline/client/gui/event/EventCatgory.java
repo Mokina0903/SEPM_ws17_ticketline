88e68aa0c7bd6c08ba6d6a3b48ea3152e4e91da6
Monika Wissmann <monika.wiszmann@gmail.com>
2018-01-21 13:11:26 +0100

    #23060  CategoryEnum added

---
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/event/EventCatgory.java
@@ -0,0 +1,12 @@
+package at.ac.tuwien.inso.sepm.ticketline.client.gui.event;
+
+public enum EventCatgory {
+        All,
+        Musical,
+        Rock,
+        Pop,
+        Kabarett,
+        Jazz,
+        Kino
+
+}
