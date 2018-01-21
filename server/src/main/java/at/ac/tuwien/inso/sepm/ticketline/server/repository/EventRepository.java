994e7d1f09c7ab1940ec78b3e5aedea1e54867ff
Verena Renner <e01527272@student.tuwien.ac.at>
2018-01-22 00:39:12 +0100

    Added all fields from the event entity to the eventrep query (group by).

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/EventRepository.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/EventRepository.java
@@ -89,18 +89,18 @@ public interface EventRepository extends JpaRepository<Event, Long>,
 
 
 
-    @Query(value = "SELECT TOP 10 e.id, e.title, e.description, e.duration, e.price, e.start_Of_Event, e.start_Of_Event_Time, e.event_date, e.end_Of_Event, e.seat_Selection, e.event_Category, e.hall_id" +
+    @Query(value = "SELECT TOP 10 e.id, e.title, e.description, e.event_Category, e.duration, e.price, e.start_Of_Event, e.start_Of_Event_Time, e.event_date, e.end_Of_Event, e.seat_Selection, e.category, e.hall_id" +
         " FROM event e join ticket t ON e.id = t.event_id " +
         " WHERE e.start_Of_Event >= :begin AND e.end_Of_Event <= :end " +
-        " GROUP BY  e.id, e.title, e.description, e.event_Category, e.price, e.start_Of_Event, e.end_Of_Event, e.start_Of_Event_Time, e.event_date, e.duration, e.seat_Selection,  e.hall_id"+
+        " GROUP BY  e.id, e.title, e.description, e.category, e.price, e.event_Category, e.start_Of_Event, e.end_Of_Event, e.start_Of_Event_Time, e.event_date, e.duration, e.seat_Selection,  e.hall_id"+
         " ORDER BY count(*)", nativeQuery = true)
     List<Event> findTopTenEventsOfMonth(@Param("begin")Timestamp beginOfMonth, @Param("end") Timestamp endOfMonth);
 
 
-    @Query(value = "SELECT TOP 10 e.id, e.title, e.description, e.duration, e.price, e.start_Of_Event, e.start_Of_Event_Time, e.event_date, e.end_Of_Event, e.seat_Selection, e.event_Category, e.hall_id" +
+    @Query(value = "SELECT TOP 10 e.id, e.title, e.description, e.event_Category, e.duration, e.price, e.start_Of_Event, e.start_Of_Event_Time, e.event_date, e.end_Of_Event, e.seat_Selection, e.category, e.hall_id" +
         " FROM event e join ticket t ON e.id = t.event_id" +
-        " WHERE e.start_Of_Event >= :begin AND e.end_Of_Event <= :end AND e.event_Category = :category" +
-        " GROUP BY  e.id, e.title, e.description, e.price, e.start_Of_Event, e.duration, e.end_Of_Event, e.seat_Selection, e.start_Of_Event_Time, e.event_date, e.event_Category, e.hall_id"+
+        " WHERE e.start_Of_Event >= :begin AND e.end_Of_Event <= :end AND e.category = :category" +
+        " GROUP BY  e.id, e.title, e.description, e.price, e.start_Of_Event, e.event_Category, e.duration, e.end_Of_Event, e.seat_Selection, e.start_Of_Event_Time, e.event_date, e.category, e.hall_id"+
         " ORDER BY count(*)", nativeQuery = true)
     List<Event> findTopTenEventsOfMonthFilteredByCategory(@Param("begin")Timestamp beginOfMonth, @Param("end") Timestamp endOfMonth, @Param("category") String category);
 
