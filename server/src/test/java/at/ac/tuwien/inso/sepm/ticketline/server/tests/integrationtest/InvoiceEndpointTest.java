11ac66f49b02d78ed8d115adaa2864e4b5e2ed7b
David <dav-it@outlook.com>
2018-01-22 02:20:45 +0100

    IntegrationTest-InvoiceEndpointTest: Task #22030 implemented Stub

--- server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/tests/integrationtest/InvoiceEndpointTest.java
+++ server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/tests/integrationtest/InvoiceEndpointTest.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.inso.sepm.ticketline.server.tests.integrationtest;
 
 import at.ac.tuwien.inso.sepm.ticketline.server.tests.base.BaseIntegrationTest;
+import org.junit.Test;
 
 public class InvoiceEndpointTest extends BaseIntegrationTest {
 
@@ -10,5 +11,10 @@ public class InvoiceEndpointTest extends BaseIntegrationTest {
     // TODO: post /invoice/update        Update given invoice
     // TODO: get /invoice/{invoiceId}        Get information about a specific invoice entry
     // TODO: get /invoice/{pageIndex}/{invoicesPerPage}        Get list of invoice entries
+
+    @Test
+    public void emptyTest(){
+        //
+    }
 
 }
